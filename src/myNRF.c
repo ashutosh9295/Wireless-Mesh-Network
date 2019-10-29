aa737bce591c2ea41c9748f45aacdf6a3fe3c32e
ashutosh9295 <ashu09295@gmail.com>
2019-10-28 22:16:19 -0400

    New radio file

--- src/myNRF.c
+++ src/myNRF.c
@@ -12,6 +12,7 @@ void sendDataPacket(uint8_t *packet, uint8_t packetLength)
   CSN_LOW();
   hal_nrf_write_tx_pload(packet, packetLength);
   CSN_HIGH();
+
 }
 
 void receiveDataPacket(uint8_t *packet)
@@ -20,4 +21,31 @@ void receiveDataPacket(uint8_t *packet)
     CSN_LOW();
     hal_nrf_read_rx_pload(packet);
     CSN_HIGH();
+
 }
+
+ParserReturnVal_t CmdSendPacket(int mode)
+{
+  uint32_t len, length, rc, data;
+  
+  if(mode != CMD_INTERACTIVE) return CmdReturnOk;
+
+  len = fetch_uint32_arg(&length);
+  if(len) {
+    printf("Must specify length value to the user\n");
+    return CmdReturnBadParameter1;
+  }
+
+  rc = fetch_uint32_arg(&data);
+  if(rc) {
+    printf("Must specify data value to the user\n");
+    return CmdReturnBadParameter1;
+  }
+
+  sendDataPacket(*data, len);
+  receiveDataPacket(*data);
+  
+  return CmdReturnOk;
+}
+
+ADD_CMD("sendPacket",CmdSendPacket,"Send packet parameters")
